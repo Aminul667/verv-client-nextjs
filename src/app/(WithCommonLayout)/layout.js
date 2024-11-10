9e5ab535fe3633a0d3ce5993837f9b75483c1690
aminul <rahathssn@gmail.com>
2024-11-10 14:46:32 +0600

    added basic setup

---
+++ src/app/(WithCommonLayout)/layout.js
@@ -0,0 +1,10 @@
+const layout = ({ children }) => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default layout;
---
+++ src/app/(WithCommonLayout)/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-4xl text-center font-bold">
+      <h1>Verv with NextJS</h1>
+      <div>
+        <Button className="bg-lime-800">Click me</Button>
+      </div>
+    </div>
+  );
+}
