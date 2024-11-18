d32df84604c1127d1cde7a0cc2ae3ede60feeb48
aminul <rahathssn@gmail.com>
2024-11-18 13:31:32 +0600

    added form

--- src/app/(WithCommonLayout)/register/page.jsx
+++ src/app/(WithCommonLayout)/register/page.jsx
@@ -2,7 +2,10 @@ import Login from "@/components/Login/Login";
 
 const LoginPage = () => {
   return (
-    <div>
+    <div className="m-auto flex flex-col items-center justify-center w-[416px] h-[664px] p-10 mt-14 gap-10 shrink-0 rounded-2xl border border-[#EEF1F3] bg-white shadow-[0px_4px_20px_rgba(0,0,0,0.08)]">
+      <h2 className="text-[#00204A] text-center font-inter text-[24px] font-semibold leading-[120%]">
+        Login to your Account
+      </h2>
       <Login />
     </div>
   );
