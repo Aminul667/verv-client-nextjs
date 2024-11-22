e03afe9f00e966d42db977b18f82d0ca05ab8cf3
aminul <rahathssn@gmail.com>
2024-11-22 13:48:45 +0600

    added single listings

--- src/zod-schemas/login.js
+++ src/zod-schemas/login.js
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const createLoginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string().min(6, "Password must be at least 6 characters"),
-  role: z.enum(["landlord", "tenant"], { required_error: "Role is required" }),
+  // role: z.enum(["landlord", "tenant"], { required_error: "Role is required" }),
 });
