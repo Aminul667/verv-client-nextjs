d9fd753e0813698a98777fd403f927f0fc1e1c82
aminul <rahathssn@gmail.com>
2024-11-25 17:14:10 +0600

    added pricing

--- src/components/SingleListing/ListingCarousel.jsx
+++ src/components/SingleListing/ListingCarousel.jsx
@@ -15,6 +15,7 @@ import propertyCarousel4 from "../../assets/images/property-carousel-img-4.png";
 import propertyCarousel5 from "../../assets/images/property-carousel-img-5.png";
 import propertyCarousel6 from "../../assets/images/property-carousel-img-6.png";
 import Image from "next/image";
+import { Button } from "../ui/button";
 
 const ListingCarousel = () => {
   const listOfCarouselImage = [
@@ -27,12 +28,12 @@ const ListingCarousel = () => {
 
   return (
     <section className="h-[982px] bg-[#EEF1F3] flex items-center justify-center">
-      <div className="w-[1216px] h-[865px] m-auto bg-green-300">
+      <div className="w-[1216px] h-[865px] m-auto">
         <p className="text-colorTextSecondary text-lg font-medium mb-[32px]">
           Home / Birmingham / Houses / Grange Road
         </p>
-        <div className="h-[811px] bg-red-400 flex items-start self-stretch gap-[32px]">
-          <div className="w-[800px] h-full bg-slate-400">
+        <div className="h-[811px] flex items-start self-stretch gap-[32px]">
+          <div className="w-[800px] h-full">
             <Carousel>
               <CarouselNext className="top-1/3 -translate-y-1/3" />
               <CarouselPrevious className="top-1/3 -translate-y-1/3" />
@@ -203,7 +204,8 @@ const ListingCarousel = () => {
               </p>
             </div>
           </div>
-          <div className="w-[384px] h-full bg-orange-400">
+          {/* property info container */}
+          <div className="w-[384px] h-full">
             <h1 className="text-colorTextPrimary text-[32px] font-semibold leading-[120%]">
               6 Bed, 3 Bathhouse
             </h1>
@@ -214,20 +216,22 @@ const ListingCarousel = () => {
             <div className="flex flex-col items-start gap-4 w-[348px] h-[187px]">
               {/* location */}
               <div className="flex items-start gap-2 w-full h-[58px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="20"
-                  viewBox="0 0 18 20"
-                  fill="none"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M9 20C12.375 20 18 13.7981 18 8.88889C18 3.97969 13.9706 0 9 0C4.02944 0 0 3.97969 0 8.88889C0 13.7981 5.625 20 9 20ZM9 12C10.6569 12 12 10.6569 12 9C12 7.34315 10.6569 6 9 6C7.34315 6 6 7.34315 6 9C6 10.6569 7.34315 12 9 12Z"
-                    fill="#50B533"
-                  />
-                </svg>
+                <div className="w-[18px] h-[20px]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="20"
+                    viewBox="0 0 18 20"
+                    fill="none"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M9 20C12.375 20 18 13.7981 18 8.88889C18 3.97969 13.9706 0 9 0C4.02944 0 0 3.97969 0 8.88889C0 13.7981 5.625 20 9 20ZM9 12C10.6569 12 12 10.6569 12 9C12 7.34315 10.6569 6 9 6C7.34315 6 6 7.34315 6 9C6 10.6569 7.34315 12 9 12Z"
+                      fill="#50B533"
+                    />
+                  </svg>
+                </div>
                 <p className="text-lg text-colorTextSecondary leading-[150%]">
                   Grange Road, Selly Oak, Birmingham, B296AP{" "}
                   <span className="font-semibold text-colorButton">
@@ -237,20 +241,22 @@ const ListingCarousel = () => {
               </div>
               {/* available from */}
               <div className="flex items-center gap-2 w-full h-[27px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="22"
-                  viewBox="0 0 18 22"
-                  fill="none"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M5.75 1.5C5.75 1.08579 5.41421 0.75 5 0.75C4.58579 0.75 4.25 1.08579 4.25 1.5V3H4C1.79086 3 0 4.79086 0 7V7.75H18V7C18 4.79086 16.2091 3 14 3H13.75V1.5C13.75 1.08579 13.4142 0.75 13 0.75C12.5858 0.75 12.25 1.08579 12.25 1.5V3H5.75V1.5ZM0 9.25H18V17.5C18 19.7091 16.2091 21.5 14 21.5H4C1.79086 21.5 0 19.7091 0 17.5V9.25ZM9 15.5C9.55229 15.5 10 15.0523 10 14.5C10 13.9477 9.55229 13.5 9 13.5C8.44771 13.5 8 13.9477 8 14.5C8 15.0523 8.44771 15.5 9 15.5ZM14 14.5C14 15.0523 13.5523 15.5 13 15.5C12.4477 15.5 12 15.0523 12 14.5C12 13.9477 12.4477 13.5 13 13.5C13.5523 13.5 14 13.9477 14 14.5ZM5 15.5C5.55228 15.5 6 15.0523 6 14.5C6 13.9477 5.55228 13.5 5 13.5C4.44772 13.5 4 13.9477 4 14.5C4 15.0523 4.44772 15.5 5 15.5Z"
-                    fill="#50B533"
-                  />
-                </svg>
+                <div className="w-[18px] h-[22px]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="22"
+                    viewBox="0 0 18 22"
+                    fill="none"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M5.75 1.5C5.75 1.08579 5.41421 0.75 5 0.75C4.58579 0.75 4.25 1.08579 4.25 1.5V3H4C1.79086 3 0 4.79086 0 7V7.75H18V7C18 4.79086 16.2091 3 14 3H13.75V1.5C13.75 1.08579 13.4142 0.75 13 0.75C12.5858 0.75 12.25 1.08579 12.25 1.5V3H5.75V1.5ZM0 9.25H18V17.5C18 19.7091 16.2091 21.5 14 21.5H4C1.79086 21.5 0 19.7091 0 17.5V9.25ZM9 15.5C9.55229 15.5 10 15.0523 10 14.5C10 13.9477 9.55229 13.5 9 13.5C8.44771 13.5 8 13.9477 8 14.5C8 15.0523 8.44771 15.5 9 15.5ZM14 14.5C14 15.0523 13.5523 15.5 13 15.5C12.4477 15.5 12 15.0523 12 14.5C12 13.9477 12.4477 13.5 13 13.5C13.5523 13.5 14 13.9477 14 14.5ZM5 15.5C5.55228 15.5 6 15.0523 6 14.5C6 13.9477 5.55228 13.5 5 13.5C4.44772 13.5 4 13.9477 4 14.5C4 15.0523 4.44772 15.5 5 15.5Z"
+                      fill="#50B533"
+                    />
+                  </svg>
+                </div>
                 <p className="text-lg text-colorTextSecondary leading-[150%]">
                   Available from:{" "}
                   <span className="font-semibold">1st January 2025</span>
@@ -258,20 +264,22 @@ const ListingCarousel = () => {
               </div>
               {/* agent */}
               <div className="flex items-center gap-2 w-full h-[27px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="25"
-                  viewBox="0 0 24 25"
-                  fill="none"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M16.1804 22.5H7.81965C5.5109 22.5 3.6393 20.714 3.6393 18.5108V13.633C3.6393 12.9248 3.34447 12.2456 2.81969 11.7448C1.60381 10.5845 1.76187 8.66205 3.15251 7.69692L9.54124 3.263C11.0071 2.24567 12.9929 2.24567 14.4588 3.263L20.8475 7.69691C22.2381 8.66205 22.3962 10.5845 21.1803 11.7448C20.6555 12.2456 20.3607 12.9248 20.3607 13.633V18.5108C20.3607 20.714 18.4891 22.5 16.1804 22.5ZM10 17.75C9.58579 17.75 9.25 18.0858 9.25 18.5C9.25 18.9142 9.58579 19.25 10 19.25H14C14.4142 19.25 14.75 18.9142 14.75 18.5C14.75 18.0858 14.4142 17.75 14 17.75H10Z"
-                    fill="#50B533"
-                  />
-                </svg>
+                <div className="w-[24px] h-[25px]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="25"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M16.1804 22.5H7.81965C5.5109 22.5 3.6393 20.714 3.6393 18.5108V13.633C3.6393 12.9248 3.34447 12.2456 2.81969 11.7448C1.60381 10.5845 1.76187 8.66205 3.15251 7.69692L9.54124 3.263C11.0071 2.24567 12.9929 2.24567 14.4588 3.263L20.8475 7.69691C22.2381 8.66205 22.3962 10.5845 21.1803 11.7448C20.6555 12.2456 20.3607 12.9248 20.3607 13.633V18.5108C20.3607 20.714 18.4891 22.5 16.1804 22.5ZM10 17.75C9.58579 17.75 9.25 18.0858 9.25 18.5C9.25 18.9142 9.58579 19.25 10 19.25H14C14.4142 19.25 14.75 18.9142 14.75 18.5C14.75 18.0858 14.4142 17.75 14 17.75H10Z"
+                      fill="#50B533"
+                    />
+                  </svg>
+                </div>
                 <p className="text-lg text-colorTextSecondary leading-[150%]">
                   Letting agent{" "}
                   <span className="font-semibold">1st January 2025</span>
@@ -279,24 +287,106 @@ const ListingCarousel = () => {
               </div>
               {/* reference */}
               <div className="flex items-center gap-2 w-full h-[27px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="22"
-                  viewBox="0 0 18 22"
-                  fill="none"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M5.75 1.5C5.75 1.08579 5.41421 0.75 5 0.75C4.58579 0.75 4.25 1.08579 4.25 1.5V3H4C1.79086 3 0 4.79086 0 7V7.75H18V7C18 4.79086 16.2091 3 14 3H13.75V1.5C13.75 1.08579 13.4142 0.75 13 0.75C12.5858 0.75 12.25 1.08579 12.25 1.5V3H5.75V1.5ZM0 9.25H18V17.5C18 19.7091 16.2091 21.5 14 21.5H4C1.79086 21.5 0 19.7091 0 17.5V9.25ZM9 15.5C9.55229 15.5 10 15.0523 10 14.5C10 13.9477 9.55229 13.5 9 13.5C8.44771 13.5 8 13.9477 8 14.5C8 15.0523 8.44771 15.5 9 15.5ZM14 14.5C14 15.0523 13.5523 15.5 13 15.5C12.4477 15.5 12 15.0523 12 14.5C12 13.9477 12.4477 13.5 13 13.5C13.5523 13.5 14 13.9477 14 14.5ZM5 15.5C5.55228 15.5 6 15.0523 6 14.5C6 13.9477 5.55228 13.5 5 13.5C4.44772 13.5 4 13.9477 4 14.5C4 15.0523 4.44772 15.5 5 15.5Z"
-                    fill="#50B533"
-                  />
-                </svg>
+                <div className="w-[18px] h-[22px]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="22"
+                    viewBox="0 0 18 22"
+                    fill="none"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M5.75 1.5C5.75 1.08579 5.41421 0.75 5 0.75C4.58579 0.75 4.25 1.08579 4.25 1.5V3H4C1.79086 3 0 4.79086 0 7V7.75H18V7C18 4.79086 16.2091 3 14 3H13.75V1.5C13.75 1.08579 13.4142 0.75 13 0.75C12.5858 0.75 12.25 1.08579 12.25 1.5V3H5.75V1.5ZM0 9.25H18V17.5C18 19.7091 16.2091 21.5 14 21.5H4C1.79086 21.5 0 19.7091 0 17.5V9.25ZM9 15.5C9.55229 15.5 10 15.0523 10 14.5C10 13.9477 9.55229 13.5 9 13.5C8.44771 13.5 8 13.9477 8 14.5C8 15.0523 8.44771 15.5 9 15.5ZM14 14.5C14 15.0523 13.5523 15.5 13 15.5C12.4477 15.5 12 15.0523 12 14.5C12 13.9477 12.4477 13.5 13 13.5C13.5523 13.5 14 13.9477 14 14.5ZM5 15.5C5.55228 15.5 6 15.0523 6 14.5C6 13.9477 5.55228 13.5 5 13.5C4.44772 13.5 4 13.9477 4 14.5C4 15.0523 4.44772 15.5 5 15.5Z"
+                      fill="#50B533"
+                    />
+                  </svg>
+                </div>
                 <p className="text-lg text-colorTextSecondary leading-[150%]">
                   Property Reference:{" "}
                   <span className="font-semibold">2191292</span>
                 </p>
+              </div>
+            </div>
+
+            {/* monthly pricing */}
+            <div className="flex flex-col items-center justify-center gap-6 p-6 w-full h-[474px] rounded-3xl bg-white mt-8">
+              {/* price */}
+              <div className="flex items-center gap-6 w-[289px] h-[81px]">
+                <div className="flex flex-col items-center -gap-1 h-full">
+                  <h1 className="text-[48px] text-colorButton font-bold leading-[120%]">
+                    &#163;950
+                  </h1>
+                  <p className="text-lg text-colorTextSecondary leading-[150%]">
+                    per month
+                  </p>
+                </div>
+                <span className="text-lg text-colorTextSecondary leading-[150%]">
+                  or
+                </span>
+                <div className="flex flex-col items-center">
+                  <h2 className="text-2xl text-colorTextSecondary font-semibold leading-[120%]">
+                    &#163;219.23
+                  </h2>
+                  <span className="text-lg text-colorTextSecondary leading-[150%]">
+                    per week
+                  </span>
+                </div>
+              </div>
+              {/* no extra fees */}
+              <div className="flex items-center gap-4 w-[314px] h-[37px]">
+                <div className="flex items-start gap-[2px] p-2 w-[129px] h-[37px] rounded-[32px] bg-[#EEF8EB]">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="21"
+                      viewBox="0 0 20 21"
+                      fill="none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M10.0003 18.8333C14.6027 18.8333 18.3337 15.1023 18.3337 10.5C18.3337 5.89759 14.6027 2.16663 10.0003 2.16663C5.39795 2.16663 1.66699 5.89759 1.66699 10.5C1.66699 15.1023 5.39795 18.8333 10.0003 18.8333ZM13.827 8.3837C14.0389 8.11123 13.9898 7.71856 13.7174 7.50664C13.4449 7.29472 13.0522 7.34381 12.8403 7.61627L9.50115 11.9095C9.4269 12.005 9.28723 12.0173 9.19733 11.9364L7.0851 10.0354C6.82853 9.80452 6.43335 9.82532 6.20244 10.0819C5.97153 10.3385 5.99233 10.7336 6.24889 10.9645L8.36113 12.8656C8.9904 13.4319 9.96808 13.3452 10.4878 12.6769L13.827 8.3837Z"
+                        fill="#50B533"
+                      />
+                    </svg>
+                  </div>
+                  <p className="text-sm text-colorTextPrimary leading-[150%]">
+                    No extra fees
+                  </p>
+                </div>
+                <div className="flex items-start gap-[2px] p-2 w-[169px] h-[37px] rounded-[32px] bg-[#EEF8EB]">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="21"
+                      viewBox="0 0 20 21"
+                      fill="none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M10.0003 18.8333C14.6027 18.8333 18.3337 15.1023 18.3337 10.5C18.3337 5.89759 14.6027 2.16663 10.0003 2.16663C5.39795 2.16663 1.66699 5.89759 1.66699 10.5C1.66699 15.1023 5.39795 18.8333 10.0003 18.8333ZM13.827 8.3837C14.0389 8.11123 13.9898 7.71856 13.7174 7.50664C13.4449 7.29472 13.0522 7.34381 12.8403 7.61627L9.50115 11.9095C9.4269 12.005 9.28723 12.0173 9.19733 11.9364L7.0851 10.0354C6.82853 9.80452 6.43335 9.82532 6.20244 10.0819C5.97153 10.3385 5.99233 10.7336 6.24889 10.9645L8.36113 12.8656C8.9904 13.4319 9.96808 13.3452 10.4878 12.6769L13.827 8.3837Z"
+                        fill="#50B533"
+                      />
+                    </svg>
+                  </div>
+                  <p className="text-sm text-colorTextPrimary leading-[150%]">
+                    Transparent pricing
+                  </p>
+                </div>
+              </div>
+              {/* proceed to rent */}
+              <div className="flex flex-col items-center gap-4 w-[336px] h-[94px]">
+                <p className="text-lg text-[#7D8A9B] font-semibold">
+                  Proceed to Rent
+                </p>
+                <Button className="w-full h-14 bg-colorButton px-4 py-2 rounded-[32px]">
+                  Rent Now
+                </Button>
               </div>
             </div>
           </div>
