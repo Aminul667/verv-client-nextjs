e4dbffc64869ddc7070e06a41f74f8f7c155737c
aminul <rahathssn@gmail.com>
2024-12-23 18:56:44 +0600

    added complete user profile

--- src/components/CompleteProfile/CompleteProfile.jsx
+++ src/components/CompleteProfile/CompleteProfile.jsx
@@ -1,51 +1,69 @@
 "use client";
 
-import InputField from "@/utils/InputField";
-import { completeProfileSchema } from "@/zod-schemas/completeProfile";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
+import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useState } from "react";
 
 const CompleteProfile = () => {
+  const [imageFiles, setImageFiles] = useState(null);
+  const methods = useForm();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    resolver: zodResolver(completeProfileSchema),
-  });
+  } = methods;
 
-  const onSubmit = (userProfileData) => {
-    console.log("Form Data: ", userProfileData);
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log(imageFiles);
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    setImageFiles(file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+    }
   };
 
   return (
-    <div className="m-auto bg-red-400 my-14 flex w-[600px] p-[40px] flex-col justify-center items-center gap-[40px] flex-shrink-0 rounded-[16px] border border-[#EEF1F3] shadow-[0px_4px_20px_rgba(0,0,0,0.08)]">
-      <div className="bg-slate-200 w-full">
-        <h2 className="text-colorTextPrimary text-center text-[24px] font-semibold leading-[120%] mb-10">
-          Complete profile
-        </h2>
+    <div className="m-auto  my-14 flex w-[600px] p-[40px] flex-col justify-center items-center flex-shrink-0 rounded-[16px] border border-[#EEF1F3] shadow-[0px_4px_20px_rgba(0,0,0,0.08)]">
+      <h2 className="text-colorTextPrimary text-center text-[24px] font-semibold leading-[120%] mb-10">
+        Complete profile
+      </h2>
+      <FormProvider {...methods}>
         <form onSubmit={handleSubmit(onSubmit)} className="w-full">
-          <InputField
+          <Input
+            className="px-3 py-1.5 rounded-md border border-gray-400 bg-white pr-10 mb-4"
+            id="firstName"
             type="text"
             placeholder="First Name"
-            registerAs="firstName"
-            register={register}
-            errors={errors}
-            className="flex w-full items-start self-stretch px-3 py-1.5 rounded-md border border-gray-400 bg-white pr-10 mb-4"
+            {...register("firstName", { required: "First Name is required" })}
           />
-          <InputField
+          {errors.firstName && (
+            <span className="text-red-500">{errors.firstName.message}</span>
+          )}
+          <Input
+            className="px-3 py-1.5 rounded-md border border-gray-400 bg-white pr-10 mb-4"
+            id="lastName"
             type="text"
             placeholder="Last Name"
-            registerAs="lastName"
-            register={register}
-            errors={errors}
-            className="flex w-full items-start self-stretch px-3 py-1.5 rounded-md border border-gray-400 bg-white pr-10 mb-4"
+            {...register("lastName", { required: "Last Name is required" })}
           />
+          {errors.lastName && (
+            <span className="text-red-500">{errors.lastName.message}</span>
+          )}
+
           <p className="text-colorTextSecondary text-[12px] font-normal leading-[140%] mb-4">
             Make sure this matches the name on your government ID. If you go by
-            another name, you can add a preferred first name.
+            another name, you can add a preferred first name.
           </p>
+
           <div className="flex items-center justify-center bg-white rounded-md border border-gray-400 mb-4">
             <div className="shrink-0 pl-3">
               <p className="text-colorTextSecondary font-normal leading-[150%]">
@@ -53,16 +71,37 @@ const CompleteProfile = () => {
               </p>
             </div>
             <div className="flex-grow">
-              <InputField
+              <Input
+                id="phoneNumber"
                 type="text"
                 placeholder="Phone Number"
-                registerAs="phoneNumber"
-                register={register}
-                errors={errors}
-                className="px-3 py-1.5"
+                {...register("phoneNumber", {
+                  required: "Phone Number is required",
+                })}
+                className="px-3 py-1.5 border-l-0"
               />
             </div>
           </div>
+          {errors.phoneNumber && (
+            <span className="text-red-500">{errors.phoneNumber.message}</span>
+          )}
+
+          <div className="flex-1">
+            <label
+              className="flex h-14 w-full cursor-pointer items-center justify-center bg-[#EEF1F3] rounded-md border border-gray-400 mb-4 shadow-sm transition-all duration-100"
+              htmlFor="image"
+            >
+              Upload image
+            </label>
+            <input
+              accept="image/*"
+              className="hidden"
+              id="image"
+              type="file"
+              onChange={(e) => handleImageChange(e)}
+            />
+          </div>
+
           <Button
             type="submit"
             className="bg-colorButton w-full rounded shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] text-lg font-semibold"
@@ -70,7 +109,7 @@ const CompleteProfile = () => {
             Agree & Continue
           </Button>
         </form>
-      </div>
+      </FormProvider>
     </div>
   );
 };
